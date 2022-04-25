7d65e458182a9c64efefe4908172c24b6c6c7b95
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-04-25 20:37:58 +0200

    Search functions

--- src/main/java/com/wiredbrain/friends/controller/FriendController.java
+++ src/main/java/com/wiredbrain/friends/controller/FriendController.java
@@ -2,6 +2,7 @@ package com.wiredbrain.friends.controller;
 
 import com.wiredbrain.friends.model.Friend;
 import com.wiredbrain.friends.service.FriendService;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,5 +37,24 @@ public class FriendController {
   @DeleteMapping("/friend/{id}")
   public void delete(@PathVariable Integer id) {
     friendService.deleteById(id);
+  }
+
+  @GetMapping("/friend/{id}")
+  public Optional<Friend> findById(@PathVariable Integer id) {
+    return friendService.findById(id);
+  }
+
+  @GetMapping("/friend/search")
+  Iterable<Friend> findByQuery(@RequestParam(value = "first", required = false) String firstName,
+      @RequestParam(value = "last", required = false) String lastName) {
+    if(firstName != null && lastName != null) {
+      return friendService.findByFirstNameAndLastName(firstName, lastName);
+    } else if(firstName != null) {
+      return friendService.findByFirstName(firstName);
+    } else if(lastName != null) {
+      return friendService.findByLastName(lastName);
+    } else {
+      return friendService.findAll();
+    }
   }
 }
--- src/main/java/com/wiredbrain/friends/service/FriendService.java
+++ src/main/java/com/wiredbrain/friends/service/FriendService.java
@@ -4,5 +4,8 @@ import com.wiredbrain.friends.model.Friend;
 import org.springframework.data.repository.CrudRepository;
 
 public interface FriendService extends CrudRepository<Friend, Integer> {
+  Iterable<Friend> findByFirstNameAndLastName(String firstName, String LastName);
+  Iterable<Friend> findByFirstName(String firstName);
+  Iterable<Friend> findByLastName(String lastName);
 
 }
